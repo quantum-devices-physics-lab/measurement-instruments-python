1e2b90f8a820eda659b7e4d6a9cca31615da94c6
Lucas Medeiros <lucamedeir@gmail.com>
2020-12-08 18:42:16 -0300

    added simulation for multiple qubit drive frequencies

--- PiPulseQt/PiPulseQt/ProcessThread.cpp
+++ PiPulseQt/PiPulseQt/ProcessThread.cpp
@@ -12,43 +12,57 @@ void ProcessThread::run()
 
 	double dtau = (mSettings.finalTau - mSettings.initialTau) / (mSettings.nSteps - 1);
 	double trueQFreq = 4.345;
-	double delta = trueQFreq - mSettings.initialQFreq;
+	double delta, Omega;
 	double  A = 1;
-	double Omega = sqrt(A*A + delta * delta);
 	double pop = 0;
 
 	double tau = mSettings.initialTau;
 
-	for (int i = 0; i < mSettings.nSteps; i++)
+	int ySteps = mSettings.nQFreqSteps;
+
+	if (!mSettings.CheckQFreqIteration)
 	{
-		{
-			QMutexLocker locker(&m_mutex);
-			if (m_stop) break;
-		}
-
-
-
-		double prob = A*A/(Omega*Omega)*sin(Omega*tau/2)*sin(Omega*tau/2)*100;
-		qDebug("prob %f", prob);
-		qDebug("tau %f", tau);
-		
-		pop = 0;
-		for (int k = 0; k < mSettings.nIter; k++)
-		{
-			double coin = (rand() % 100);
-
-			if (coin < prob)
-			{
-				pop += 1.0f;
-			}
-		}
-		pop /= mSettings.nIter;
-
-
-		tau += dtau;
-
-		emit signalDataPoint(i,0, pop);
+		ySteps = 1;
 	}
+
+	double qFreq = mSettings.initialQFreq;
+	double dFreq = (mSettings.finalQFreq - mSettings.initialQFreq)/(mSettings.nQFreqSteps-1);
+
+	for (int j = 0; j < ySteps; j++)
+	{
+		delta = trueQFreq - qFreq;
+		Omega = sqrt(A*A + delta * delta);
+
+		for (int i = 0; i < mSettings.nSteps; i++)
+		{
+			{
+				QMutexLocker locker(&m_mutex);
+				if (m_stop) break;
+			}
+
+			double prob = A * A / (Omega*Omega)*sin(Omega*tau / 2)*sin(Omega*tau / 2) * 100;
+
+			pop = 0;
+			for (int k = 0; k < mSettings.nIter; k++)
+			{
+				double coin = (rand() % 100);
+
+				if (coin < prob)
+				{
+					pop += 1.0f;
+				}
+			}
+			pop /= mSettings.nIter;
+
+
+			tau += dtau;
+
+			emit signalDataPoint(i, j, pop);
+		}
+		qFreq += dFreq;
+	}
+
+	
 
 
 }
