1df910edd6d28a819604cd35203e8d8135ea6d48
Lucas Medeiros <lucamedeir@gmail.com>
2021-01-14 10:50:56 -0300

    added a visa connection

--- OneToneQt/OneToneQt/main.cpp
+++ OneToneQt/OneToneQt/main.cpp
@@ -1,12 +1,38 @@
+
+#define _USE_MATH_DEFINES
 #include "OneToneQt.h"
 #include <QtWidgets/QApplication>
 #include "PythonThread.h"
 #include "datatype.h"
-
+#include "visa.h"
 
 
 int main(int argc, char *argv[])
 {
+	int error;
+	ViSession session, vi;
+
+	ViChar* resrcDefault = "TCPIP0::169.254.101.103::inst0::INSTR";
+	ViChar* resrc = resrcDefault;
+	error = viOpenDefaultRM(&session);
+	error = viOpen(session, resrc, VI_NO_LOCK, 10000, &vi);
+	if (error != VI_SUCCESS)
+	{
+		viClose(session);
+		qDebug("Visa session failed");
+		return 1;
+	}
+
+	// Query the instrument identity
+	error = viPrintf(vi, "*IDN?\n");
+	ViChar buffer[5000];
+	error = viScanf(vi, "%t", buffer);
+	qDebug("*IDN? -> %s", buffer);
+
+	error = viClose(vi);
+	error = viClose(session);
+
+
 
     QApplication a(argc, argv);	
     OneToneQt w;
